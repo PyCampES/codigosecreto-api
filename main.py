cbd9d3e4e2feca7e44cda7b4268b9714bccd493e
Israel Saeta Pérez <dukebody@gmail.com>
2022-04-17 16:08:31 +0200

    Switch to txt file.

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import random
 
 
 this_file_folder = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(this_file_folder, "words.json"), "r") as input:
-    data = json.load(input)
+with open(os.path.join(this_file_folder, "words.txt"), "r") as input:
+    available_words = [word.replace("\n", "") for word in input.readlines()]
 
-available_words = data["words"]
 
 app = FastAPI()
 
@@ -26,10 +25,10 @@ async def words(seed):
     white_words = selected_words[words_per_team_count + 1 :]
 
     words = (
-        [{"name": word["name"], "role": "blue"} for word in blue_words]
-        + [{"name": word["name"], "role": "red"} for word in red_words]
-        + [{"name": word["name"], "role": "black"} for word in black_words]
-        + [{"name": word["name"], "role": "white"} for word in white_words]
+        [{"name": word, "role": "blue"} for word in blue_words]
+        + [{"name": word, "role": "red"} for word in red_words]
+        + [{"name": word, "role": "black"} for word in black_words]
+        + [{"name": word, "role": "white"} for word in white_words]
     )
 
     random.shuffle(words)
