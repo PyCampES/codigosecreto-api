49bb8c8cafbecc7ff4dc4ba51256ca8c1f09d023
Israel Saeta Pérez <dukebody@gmail.com>
2022-04-18 11:00:47 +0200

    Añadir más palabras y arreglar cuentas de color.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import json
 import os
 import random
+import math
 
 
 this_file_folder = os.path.dirname(os.path.realpath(__file__))
@@ -15,14 +16,14 @@ app = FastAPI()
 @app.get("/{seed}/")
 async def words(seed):
     random.seed(seed)
-    selected_words = random.sample(available_words, k=min(len(available_words), 25))
+    selected_words = random.sample(available_words, k=25)
 
-    words_per_team_count = len(selected_words) // 3
+    words_per_team_count = 5
 
     blue_words = selected_words[:words_per_team_count]
     red_words = selected_words[words_per_team_count : words_per_team_count * 2]
     black_words = [selected_words[words_per_team_count * 2]]
-    white_words = selected_words[words_per_team_count + 1 :]
+    white_words = selected_words[words_per_team_count * 2 + 1 :]
 
     words = (
         [{"name": word, "role": "blue"} for word in blue_words]
